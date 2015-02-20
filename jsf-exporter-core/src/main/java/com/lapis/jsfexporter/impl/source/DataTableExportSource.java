071fb13c276e7b5088e3fc983f30572ae508b20a
Richard DiCroce <rdicroce@lapis.com>
2015-02-20 16:01:03 -0500

    Update Maven plugin versions. Update copyright headers.

--- jsf-exporter-core/src/main/java/com/lapis/jsfexporter/impl/source/DataTableExportSource.java
+++ jsf-exporter-core/src/main/java/com/lapis/jsfexporter/impl/source/DataTableExportSource.java
@@ -2,7 +2,7 @@
  * #%L
  * Lapis JSF Exporter Core
  * %%
- * Copyright (C) 2013 Lapis Software Associates
+ * Copyright (C) 2013 - 2015 Lapis Software Associates
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
